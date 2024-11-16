e186629dc2c76048ec180d0b758ab6edd7839d38
Bruno S. Almeida <bschwell@Wqstudent.42porto.com>
2024-11-16 12:27:52 +0000

    optimize lines numbers

--- src/handle_malloc.c
+++ src/handle_malloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_malloc.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tcosta-f <tcosta-f@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: bschwell <bschwell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 22:18:13 by tcosta-f          #+#    #+#             */
-/*   Updated: 2024/11/12 22:36:36 by tcosta-f         ###   ########.fr       */
+/*   Updated: 2024/11/16 12:27:23 by bschwell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,11 @@ void	ft_free_tokens(t_token *tokens)
 {
 	int	i;
 
-	i = 0;
+	i = -1;
 	if (!tokens)
 		return ;
-	while (tokens[i].value)
-	{
+	while (tokens[++i].value)
 		free(tokens[i].value);
-		i++;
-	}
 	free(tokens);
 	return ;
 }
@@ -51,12 +48,9 @@ void	ft_free_cmd_ready(char **cmd_ready)
 
 	if (!cmd_ready)
 		return ;
-	i = 0;
-	while (cmd_ready[i])
-	{
+	i = -1;
+	while (cmd_ready[++i])
 		free(cmd_ready[i]);
-		i++;
-	}
 	free(cmd_ready);
 	return ;
 }
