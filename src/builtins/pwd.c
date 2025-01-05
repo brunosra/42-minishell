9438199c5ad6e0d321bfb9e70561ee7cb2b57c2a
Bruno A Almeida <bralmeida1980@gmail.com>
2025-01-05 15:06:49 +0000

    styling

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: bschwell <student@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 09:13:42 by bschwell          #+#    #+#             */
-/*   Updated: 2025/01/01 12:19:18 by bschwell         ###   ########.fr       */
+/*   Updated: 2025/01/05 15:06:46 by bschwell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ extern volatile sig_atomic_t g_interrupt;
 /**
  * @brief	pwd - print name of current/working directory
  * 
- * @param ms minishell pointer
- * @return int 
+ * @param 	ms minishell pointer
+ * @return 	int 
  */
 
 int	ft_builtin_pwd()
