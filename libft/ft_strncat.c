173bed78f78c65b28e6964b3658c332eacccc31c
Bruno A Almeida <bralmeida1980@gmail.com>
2025-01-06 12:10:14 +0000

    fix if error

--- libft/ft_strncat.c
+++ libft/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: bschwell <student@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 19:04:22 by tcosta-f          #+#    #+#             */
-/*   Updated: 2025/01/05 19:06:44 by bschwell         ###   ########.fr       */
+/*   Updated: 2025/01/06 11:45:26 by bschwell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char *ft_strncat(char *dest, const char *src, size_t dest_size) {
 	size_t dest_len;
     size_t i;
 
+	i = 0;
 	dest_len = ft_strlen(dest);
     if (dest_len >= dest_size - 1)
-        return dest;
-	i = 0;
+        return (dest);
     while (i < dest_size - dest_len - 1 && src[i] != '\0')
 	{
         dest[dest_len + i] = src[i];
