d1dd1fbc67af1f3284c57c329b3c43a2816f9781
Tiago Sousa Neves <tiagosusaneves@gamil.com>
2025-02-24 06:55:33 +0000

    agora da o comando "<< EOF cat"

--- src/lexing_input.c
+++ src/lexing_input.c
@@ -6,7 +6,7 @@
 /*   By: tcosta-f <tcosta-f@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 02:49:34 by tcosta-f          #+#    #+#             */
-/*   Updated: 2025/02/24 03:45:44 by tcosta-f         ###   ########.fr       */
+/*   Updated: 2025/02/24 06:51:59 by tcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -242,7 +242,7 @@ t_type	ft_get_token_type(char *str, t_type prev_type)
 	static bool inverted = false;
 
 	type = ft_check_redirection(str);
-	if (type == TOKEN_OUTPUT_REDIRECT && prev_type == TOKEN_OPERATOR)
+	if ((type == TOKEN_OUTPUT_REDIRECT || type == TOKEN_HEREDOC) && (prev_type == TOKEN_OPERATOR || prev_type == TOKEN_NULL))
 		inverted = true;
 	if (type != TOKEN_COMMAND)
 	return (type);
