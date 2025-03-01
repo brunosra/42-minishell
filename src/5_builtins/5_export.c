1e448209eddbb2137e9abd13c7e9c096178b758b
Bruno A Almeida <bralmeida1980@gmail.com>
2025-03-01 19:59:17 +0000

    export works

--- src/5_builtins/5_export.c
+++ src/5_builtins/5_export.c
@@ -6,7 +6,7 @@
 /*   By: bschwell <student@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 21:13:47 by bschwell          #+#    #+#             */
-/*   Updated: 2025/03/01 18:52:45 by bschwell         ###   ########.fr       */
+/*   Updated: 2025/03/01 19:57:16 by bschwell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int			ft_builtin_export_check(char **args/* , t_minishell *ms */);
 void		ft_builtin_export(char **args, t_minishell *ms);
 static int	ft_export_1_arg(t_minishell *ms);
 
-// TODO: implement
 int	ft_builtin_export_check(char **args/* , t_minishell *ms */)
 {
 	int		count;
@@ -25,6 +24,49 @@ int	ft_builtin_export_check(char **args/* , t_minishell *ms */)
 	while (args[count] != NULL)
 		count++;
 	// printf("export check: %s\n", ms->env.envp[0]);
+	return (0);
+}
+
+int	ft_export_add_var(char *arg, t_minishell *ms)
+{
+	char	**new_env;
+	char	*new_var;
+	int		count;
+	int		i;
+
+	count = 0;
+	while (ms->env.envp[count])
+		count++;
+	if (!ft_strchr(arg, '='))
+	{
+		new_var = malloc(ft_strlen(arg) + 2);
+		if (!new_var)
+			return (-1);
+		ft_strcpy(new_var, arg);
+		ft_strcat(new_var, "=");
+	}
+	else
+	{
+		new_var = ft_strdup(arg);
+		if (!new_var)
+			return (-1);
+	}
+	new_env = malloc((count + 2) * sizeof(char *));
+	if (!new_env)
+	{
+		free(new_var);
+		return (-1);
+	}
+	i = 0;
+	while (i < count)
+	{
+		new_env[i] = ms->env.envp[i];
+		i++;
+	}
+	new_env[count] = new_var;
+	new_env[count + 1] = NULL;
+	free(ms->env.envp);
+	ms->env.envp = new_env;
 	return (0);
 }
 
@@ -53,10 +95,11 @@ int	ft_valid_export_arg(const char *arg)
 	return (1);
 }
 
-static int	ft_process_export(char* str)
+static int	ft_process_export(char* str, t_minishell *ms)
 {
-	if (str)
+	if (!str)
 		return (0);
+	ft_export_add_var(str, ms);
 	return (1);
 }
 
@@ -74,7 +117,9 @@ void	ft_builtin_export(char **args, t_minishell *ms)
 			if (ft_valid_export_arg(args[i]) == 0)
 				ft_exit_code(1);
 			else
-				ft_process_export(args[i]);
+			{
+				ft_process_export(args[i], ms);
+			}
 		}
 	}
 	
