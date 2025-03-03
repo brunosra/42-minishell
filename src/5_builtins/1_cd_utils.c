73258ac572e9e8e8f5001819827d65e9ee1f4cf9
Tiago Sousa Neves <tiagosusaneves@gamil.com>
2025-03-03 18:27:39 +0000

    nada de mais

--- src/5_builtins/1_cd_utils.c
+++ src/5_builtins/1_cd_utils.c
@@ -6,7 +6,7 @@
 /*   By: tcosta-f <tcosta-f@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 11:27:32 by bschwell          #+#    #+#             */
-/*   Updated: 2025/02/27 02:12:31 by tcosta-f         ###   ########.fr       */
+/*   Updated: 2025/03/03 18:26:45 by tcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,7 @@ int	ft_builtin_cd_check(char **args, t_minishell *ms)
 		return (ret);
 	if (chdir(res_p) != 0)
 	{
-		ft_putstr_fd("Minishell: cd: ", STDERR_FILENO);
+		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
 		ft_putstr_fd(args[1], STDERR_FILENO);
 		ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
 		return (1);
